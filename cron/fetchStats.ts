8e1ddfc9691e9524c1dd6a1161173b4e5012d2ea
Ben Feldhake <BenF117@gmail.com>
2023-01-04 13:49:06 -0600

    read from google sheet

--- cron/fetchStats.ts
+++ cron/fetchStats.ts
@@ -1,6 +1,6 @@
 import { getPlayerDataThrottled } from './slippi'
-// import { GoogleSpreadsheet } from 'google-spreadsheet';
-// import creds from '../secrets/creds.json';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import creds from '../secrets/creds.json';
 import * as syncFs from 'fs';
 import * as path from 'path';
 import util from 'util';
@@ -10,29 +10,29 @@ import { exec } from 'child_process';
 const fs = syncFs.promises;
 const execPromise = util.promisify(exec);
 
-var playerCodes = [
-    "PANTS#0", "LAWN#628", "SAMU#623", "BG#0", "HUNA#300"
-];
+// var playerCodes = [
+//     "PANTS#0", "LAWN#628", "SAMU#623", "BG#0", "HUNA#300"
+// ];
 
-var playerTags = [
-  "Pants", "Lawngarms", "samusown", "BuddyGuysman", "3Huna"
-];
+// var playerTags = [
+//   "Pants", "Lawngarms", "samusown", "BuddyGuysman", "3Huna"
+// ];
 
-// const getSheetData = async (): Promise<string[][]> => {
-//   const doc = new GoogleSpreadsheet(settings.spreadsheetID);
-//   await doc.useServiceAccountAuth(creds);
-//   await doc.loadInfo(); // loads document properties and worksheets
-//   const sheet = doc.sheetsByIndex[0];
-//   const rows = (await sheet.getRows()); // remove header row, changed to 0 from 1
-//   const codes = [...new Set(rows.map((r) => r._rawData[1]).filter(r => r !== ''))] as string[]
-//   const tags = [...new Set(rows.map((r) => r._rawData[2]).filter(r => r !== ''))] as string[]
-//   return [codes, tags]
-// };
+const getSheetData = async (): Promise<string[][]> => {
+  const doc = new GoogleSpreadsheet(settings.spreadsheetID);
+  await doc.useServiceAccountAuth(creds);
+  await doc.loadInfo(); // loads document properties and worksheets
+  const sheet = doc.sheetsByIndex[0];
+  const rows = (await sheet.getRows()); // remove header row, changed to 0 from 1
+  const codes = [...new Set(rows.map((r) => r._rawData[1]).filter(r => r !== ''))] as string[]
+  const tags = [...new Set(rows.map((r) => r._rawData[2]).filter(r => r !== ''))] as string[]
+  return [codes, tags]
+};
 
 const getPlayers = async () => {
-  // const sheetData = await getSheetData()
-  const codes = playerCodes;
-  const tags = playerTags;
+  const sheetData = await getSheetData()
+  const codes = sheetData[0];
+  const tags = sheetData[1];
   console.log(`Found ${codes.length} player codes`)
   const allData = codes.map(code => getPlayerDataThrottled(code))
   const results = await Promise.all(allData.map(p => p.catch(e => e)));
