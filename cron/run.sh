ebe5ac8f0a588e5c6757d3accceb7e82c5c00ceb
root <timtempor@sbcglobal.net>
2023-01-05 00:09:46 +0000

    updated run.sh

--- cron/run.sh
+++ cron/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
+
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.."
-node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
+/root/.nvm/versions/node/v18.12.0/bin/node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
